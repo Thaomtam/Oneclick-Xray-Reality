3da09288576bf0f80b2f4ae9b18273f429f5bc8d
Thảo mai tắm <109426941+Thaomtam@users.noreply.github.com>
2023-09-07 09:44:43 +0700

    Update vision.sh

--- vision.sh
+++ vision.sh
@@ -4,13 +4,20 @@ apt-get install -y jq
 apt-get install -y openssl
 apt-get install -y qrencode
 
+# Ask for time zone
+timedatectl set-timezone Asia/Ho_Chi_Minh && \
+apt install ntp && \
+timedatectl set-ntp on && \
+sysctl -w net.core.rmem_max=16777216 && \
+sysctl -w net.core.wmem_max=16777216
+
 read -p "Enter serverNames: " sni
 
 read -p "Enter uuid: " uuid
 
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta
 
-json=$(curl -s https://raw.githubusercontent.com/Thaomtam/Oneclick-Xray-Reality/main/xtls-rprx-vision.json?token=GHSAT0AAAAAACHFDCR2U4C7UJNNFTBYYZEYZHYJ7UA)
+json=$(curl -s https://raw.githubusercontent.com/Thaomtam/Oneclick-Xray-Reality/main/xtls-rprx-vision.json)
 
 keys=$(xray x25519)
 pk=$(echo "$keys" | awk '/Private key:/ {print $3}')
@@ -19,7 +26,7 @@ serverIp=$(curl -s ifconfig.me)
 uuid=$uuid
 shortId=$(openssl rand -hex 8)
 sni=$sni
-url="vless://$uuid@$serverIp:443?path=%2F&security=reality&encryption=none&pbk=$pub&fp=chrome&type=http&sni=$sni&sid=$shortId#h2-huy"
+url="vless://$uuid@$serverIp:443?path=%2F&security=reality&encryption=none&pbk=$pub&fp=chrome&type=http&sni=$sni&sid=$shortId#Thời-Tiết-Openwrt"
 
 newJson=$(echo "$json" | jq \
     --arg sni "$sni" \
@@ -34,12 +41,6 @@ echo "$newJson" | sudo tee /usr/local/etc/xray/config.json >/dev/null
 # Configure Nginx & Geosite and Geoip
 curl -Lo /usr/local/share/xray/geoip.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat && curl -Lo /usr/local/share/xray/geosite.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat && systemctl restart xray
 
-# Ask for time zone
-timedatectl set-timezone Asia/Ho_Chi_Minh && \
-apt install ntp && \
-timedatectl set-ntp on && \
-sysctl -w net.core.rmem_max=16777216 && \
-sysctl -w net.core.wmem_max=16777216
 
 echo "$url"
 
