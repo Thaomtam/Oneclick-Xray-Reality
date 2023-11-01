0cad4b39c01248db34bb5996b60d43a173cb0909
Thảo mai tắm <109426941+Thaomtam@users.noreply.github.com>
2023-11-01 19:30:47 +0700

    Update Reality.sh

--- Reality.sh
+++ Reality.sh
@@ -2,7 +2,13 @@
 
 # Install snapd
 apt update -y && \
-apt install -y snapd
+apt install -y socat
+curl https://get.acme.sh | sh
+source ~/.bashrc
+acme.sh --upgrade --auto-upgrade
+acme.sh --set-default-ca --server letsencrypt
+
+chown -R nobody:nogroup /etc/ssl/private
 
 # Ask for domain
 read -p "MY DOMAIN: " domain
@@ -13,29 +19,10 @@ read -p "Bug SNI: " sni
 # Ask for SNI
 read -p "UUID: " id
 
-# Install certbot
-snap install core
-snap install --classic certbot
-ln -s /snap/bin/certbot /usr/bin/certbot
-
-# Obtain SSL certificate
-certbot certonly --standalone --register-unsafely-without-email -d $domain
-
-# Copy SSL certificate files
-cp /etc/letsencrypt/archive/*/fullchain*.pem /etc/ssl/private/fullchain.cer
-cp /etc/letsencrypt/archive/*/privkey*.pem /etc/ssl/private/private.key
-chown -R nobody:nogroup /etc/ssl/private
-chmod -R 0644 /etc/ssl/private/*
-
-# Schedule automatic renewal
-printf "0 0 1 * * /root/update_certbot.sh\n" > update && crontab update && rm update
-cat > /root/update_certbot.sh << EOF
-#!/usr/bin/env bash
-certbot renew --pre-hook "systemctl stop nginx" --post-hook "systemctl start nginx"
-cp /etc/letsencrypt/archive/*/fullchain*.pem /etc/ssl/private/fullchain.cer
-cp /etc/letsencrypt/archive/*/privkey*.pem /etc/ssl/private/private.key
-EOF
-chmod +x update_certbot.sh
+acme.sh --issue -d $domain --standalone --keylength ec-256
+acme.sh --install-cert -d $domain --ecc \
+--fullchain-file /etc/ssl/private/fullchain.cer \
+--key-file /etc/ssl/private/private.key
 
 # Install Nginx
 apt install -y gnupg2 ca-certificates lsb-release ubuntu-keyring && curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor > /usr/share/keyrings/nginx-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/mainline/ubuntu `lsb_release -cs` nginx" > /etc/apt/sources.list.d/nginx.list && echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" > /etc/apt/preferences.d/99nginx && apt update -y && apt install -y nginx && mkdir -p /etc/systemd/system/nginx.service.d && echo -e "[Service]\nExecStartPost=/bin/sleep 0.1" > /etc/systemd/system/nginx.service.d/override.conf && systemctl daemon-reload
@@ -74,8 +61,6 @@ curl -Lo /usr/local/share/xray/geoip.dat https://cdn.jsdelivr.net/gh/Loyalsoldie
 
 # Ask for time zone
 timedatectl set-timezone Asia/Ho_Chi_Minh && \
-apt install ntp && \
-timedatectl set-ntp on && \
 sysctl -w net.core.rmem_max=16777216 && \
 sysctl -w net.core.wmem_max=16777216
 
